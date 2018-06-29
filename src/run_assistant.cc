1ae38d20fbb817620cae1ce22642be6109c577b6
subjectxbj <subjectxbj@163.com>
2018-06-29 16:04:17 +0800

    set conversation state

--- src/run_assistant.cc
+++ src/run_assistant.cc
@@ -68,93 +68,94 @@ static const std::string kDeviceInstanceId = "default";
 static const std::string kDeviceModelId = "default";
 
 bool verbose = false;
+std::string mConversationState;
 
 // Creates a channel to be connected to Google.
 std::shared_ptr<Channel> CreateChannel(const std::string& host) {
-  std::ifstream file("robots.pem");
-  std::stringstream buffer;
-  buffer << file.rdbuf();
-  std::string roots_pem = buffer.str();
+	std::ifstream file("robots.pem");
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	std::string roots_pem = buffer.str();
 
-  if (verbose) {
-    std::clog << "assistant_sdk robots_pem: " << roots_pem << std::endl;
-  }
-  ::grpc::SslCredentialsOptions ssl_opts = {roots_pem, "", ""};
-  auto creds = ::grpc::SslCredentials(ssl_opts);
-  std::string server = host + ":443";
-  if (verbose) {
-    std::clog << "assistant_sdk CreateCustomChannel(" << server << ", creds, arg)"
-      << std::endl << std::endl;
-  }
-  ::grpc::ChannelArguments channel_args;
-  return CreateCustomChannel(server, creds, channel_args);
+	if (verbose) {
+		std::clog << "assistant_sdk robots_pem: " << roots_pem << std::endl;
+	}
+	::grpc::SslCredentialsOptions ssl_opts = {roots_pem, "", ""};
+	auto creds = ::grpc::SslCredentials(ssl_opts);
+	std::string server = host + ":443";
+	if (verbose) {
+		std::clog << "assistant_sdk CreateCustomChannel(" << server << ", creds, arg)"
+			<< std::endl << std::endl;
+	}
+	::grpc::ChannelArguments channel_args;
+	return CreateCustomChannel(server, creds, channel_args);
 }
 
 void PrintUsage() {
-  std::cerr << "Usage: ./run_assistant "
-            << "[--audio_input [<" << kALSAAudioInput << ">|<audio_file>] OR --text_input <string>] "
-            << "--credentials_file <credentials_file> "
-            << "[--credentials_type <" << kCredentialsTypeUserAccount << ">] "
-            << "[--api_endpoint <API endpoint>] "
-            << "[--locale <locale>]"
-            << std::endl;
+	std::cerr << "Usage: ./run_assistant "
+		<< "[--audio_input [<" << kALSAAudioInput << ">|<audio_file>] OR --text_input <string>] "
+		<< "--credentials_file <credentials_file> "
+		<< "[--credentials_type <" << kCredentialsTypeUserAccount << ">] "
+		<< "[--api_endpoint <API endpoint>] "
+		<< "[--locale <locale>]"
+		<< std::endl;
 }
 
 bool GetCommandLineFlags(
-    int argc, char** argv, std::string* audio_input, std::string* text_input,
-    std::string* credentials_file_path, std::string* credentials_type,
-    std::string* api_endpoint, std::string* locale) {
-  const struct option long_options[] = {
-    {"audio_input",      required_argument, nullptr, 'i'},
-    {"text_input",       required_argument, nullptr, 't'},
-    {"credentials_file", required_argument, nullptr, 'f'},
-    {"credentials_type", required_argument, nullptr, 'c'},
-    {"api_endpoint",     required_argument, nullptr, 'e'},
-    {"locale",           required_argument, nullptr, 'l'},
-    {"verbose",          no_argument, nullptr, 'v'},
-    {nullptr, 0, nullptr, 0}
-  };
-  *api_endpoint = ASSISTANT_ENDPOINT;
-  while (true) {
-    int option_index;
-    int option_char =
-        getopt_long(argc, argv, "i:t:f:c:e:l:v", long_options, &option_index);
-    if (option_char == -1) {
-      break;
-    }
-    switch (option_char) {
-      case 'i':
-        *audio_input = optarg;
-        break;
-      case 't':
-        *text_input = optarg;
-      case 'f':
-        *credentials_file_path = optarg;
-        break;
-      case 'c':
-        *credentials_type = optarg;
-        if (*credentials_type != kCredentialsTypeUserAccount) {
-          std::cerr << "Invalid credentials_type: \"" << *credentials_type
-                    << "\". Should be \"" << kCredentialsTypeUserAccount
-                    << "\"" << std::endl;
-          return false;
-        }
-        break;
-      case 'e':
-        *api_endpoint = optarg;
-        break;
-      case 'l':
-        *locale = optarg;
-        break;
-      case 'v':
-        verbose = true;
-        break;
-      default:
-        PrintUsage();
-        return false;
-    }
-  }
-  return true;
+	int argc, char** argv, std::string* audio_input, std::string* text_input,
+	std::string* credentials_file_path, std::string* credentials_type,
+	std::string* api_endpoint, std::string* locale) {
+		
+	const struct option long_options[] = {
+		{"audio_input",      required_argument, nullptr, 'i'},
+		{"text_input",       required_argument, nullptr, 't'},
+		{"credentials_file", required_argument, nullptr, 'f'},
+		{"credentials_type", required_argument, nullptr, 'c'},
+		{"api_endpoint",     required_argument, nullptr, 'e'},
+		{"locale",           required_argument, nullptr, 'l'},
+		{"verbose",          no_argument, nullptr, 'v'},
+		{nullptr, 0, nullptr, 0}
+	};
+	*api_endpoint = ASSISTANT_ENDPOINT;
+	while (true) {
+		int option_index;
+		int option_char = getopt_long(argc, argv, "i:t:f:c:e:l:v", long_options, &option_index);
+		if (option_char == -1) {
+			break;
+		}
+		switch (option_char) {
+			case 'i':
+				*audio_input = optarg;
+				break;
+			case 't':
+				*text_input = optarg;
+			case 'f':
+				*credentials_file_path = optarg;
+				break;
+			case 'c':
+				*credentials_type = optarg;
+				if (*credentials_type != kCredentialsTypeUserAccount) {
+					std::cerr << "Invalid credentials_type: \"" << *credentials_type
+						<< "\". Should be \"" << kCredentialsTypeUserAccount
+						<< "\"" << std::endl;
+					return false;
+				}
+				break;
+			case 'e':
+				*api_endpoint = optarg;
+				break;
+			case 'l':
+				*locale = optarg;
+				break;
+			case 'v':
+				verbose = true;
+				break;
+			default:
+				PrintUsage();
+				return false;
+		}
+	}
+	return true;
 }
 
 AssistRequest MakeAssistRequestConfig(std::string locale){
@@ -167,6 +168,9 @@ AssistRequest MakeAssistRequestConfig(std::string locale){
     std::cout << "Using locale " << locale << std::endl;
     // Set the DialogStateIn of the AssistRequest
     assist_config->mutable_dialog_state_in()->set_language_code(locale);
+	if (mConversationState.size()>0) {
+		assist_config->mutable_dialog_state_in()->set_conversation_state(mConversationState);
+	}
     // Set the DeviceConfig of the AssistRequest
     assist_config->mutable_device_config()->set_device_id(kDeviceInstanceId);
     assist_config->mutable_device_config()->set_device_model_id(kDeviceModelId);
@@ -209,10 +213,10 @@ bool StartDialog(std::string locale,
 	grpc::ClientContext context;
 	context.set_fail_fast(false);
 	context.set_credentials(call_credentials);
-	  
+	
 	std::shared_ptr<ClientReaderWriter<AssistRequest, AssistResponse>>
-            stream(std::move(assistant->Assist(&context)));
-	  
+		stream(std::move(assistant->Assist(&context)));
+	
 	// Reset Audio Input
 	audio_input.reset(new AudioInputALSA());
 
@@ -220,17 +224,17 @@ bool StartDialog(std::string locale,
 		[stream, &request_audio_in](std::shared_ptr<std::vector<unsigned char>> data) {
 			request_audio_in.set_audio_in(&((*data)[0]), data->size());
 			stream->Write(request_audio_in);
-                        std::cout << "==>AssistRequest.audio_in" << std::endl;
+			std::cout << "==>AssistRequest.audio_in" << std::endl;
 		}
 	);
 	audio_input->AddStopListener([stream]() {
 		stream->WritesDone();
-                std::cout << "==>AssistRequest.audio_in END" << std::endl;
+		std::cout << "==>AssistRequest.audio_in END" << std::endl;
 	});
 
 	stream->Write(MakeAssistRequestConfig(locale));
-        std::cout << "==>AssistRequest.config" << std::endl;	
-        PlaySoundCue("ful_ui_wakesound.wav");
+	std::cout << "==>AssistRequest.config" << std::endl;	
+	PlaySoundCue("ful_ui_wakesound.wav");
  
 	// Start Audio Input Thread
 	audio_input->Start();
@@ -238,67 +242,57 @@ bool StartDialog(std::string locale,
   
 	// Start reading response
 	while (stream->Read(&response)) {  // Returns false when no more to read.
-                std::cout << "*****GET A RESPONSE" <<std::endl;
-/*
-		if (audio_input->IsRunning()) {
-			if(response.has_audio_out()) {
-				std::cout << "<==AssistResponse.audio_out" <<std::endl;
-	         		audio_input->Stop();
-                                b_cont = false;
-			}
-			if(response.event_type() == AssistResponse_EventType_END_OF_UTTERANCE) {
-				std::cout << "<==AssistResponse.event_type.END_OF_UTTERANCE" <<std::endl;
-				audio_input->Stop();
-                                b_cont = false;
-			}
+	
+	    std::string conversationState = response.dialog_state_out().conversation_state();
+		std::cout << "<==Converstation State: " << conversationState <<std::endl;
+		
+		if (conversationState.size())
+		{
+			mConversationState = conversationState;
 		}
-*/
-                std::cout << "Converstation State: " << response.dialog_state_out().conversation_state() <<std::endl;
-                if(response.event_type() == AssistResponse_EventType_END_OF_UTTERANCE) {
-                    std::cout << "<==AssistResponse.event_type.END_OF_UTTERANCE" <<std::endl;
-                    audio_input->Stop();
-                    PlaySoundCue("ful_ui_endpointing.wav");
-                }else if (response.event_type() == AssistResponse_EventType_EVENT_TYPE_UNSPECIFIED) {
-                    std::cout << "<==AssistResponse.event_type.EVENT_TYPE_UNSPECIFIED" <<std::endl;
-                }
+		
+		if(response.event_type() == AssistResponse_EventType_END_OF_UTTERANCE) {
+			std::cout << "<==AssistResponse.event_type.END_OF_UTTERANCE" <<std::endl;
+			audio_input->Stop();
+			PlaySoundCue("ful_ui_endpointing.wav");
+		}else if (response.event_type() == AssistResponse_EventType_EVENT_TYPE_UNSPECIFIED) {
+			std::cout << "<==AssistResponse.event_type.EVENT_TYPE_UNSPECIFIED" <<std::endl;
+		}
  
 		if (response.dialog_state_out().microphone_mode() == DialogStateOut_MicrophoneMode_CLOSE_MICROPHONE) {
-		    std::cout << "<==AssistResponse.dialog_state_out.microphone_mode.CLOSE_MICROPHONE" <<std::endl;
-                    b_cont = false;
+			std::cout << "<==AssistResponse.dialog_state_out.microphone_mode.CLOSE_MICROPHONE" <<std::endl;
+			b_cont = false;
 		}
 		else if (response.dialog_state_out().microphone_mode() == DialogStateOut_MicrophoneMode_DIALOG_FOLLOW_ON) {
-		    std::cout << "<==AssistResponse.dialog_state_out.microphone_mode.DIALOG_FOLLOW_ON" <<std::endl;
-                    b_cont = true;
+			std::cout << "<==AssistResponse.dialog_state_out.microphone_mode.DIALOG_FOLLOW_ON" <<std::endl;
+			b_cont = true;
 		}
-                if(response.has_audio_out()) {
-                    std::cout << "<==AssistResponse.audio_out" <<std::endl;
-                }
-              
 	
 		// Playback the response audio
 		if (response.has_audio_out()) {
-		    std::shared_ptr<std::vector<unsigned char>>
+			std::cout << "<==AssistResponse.audio_out" <<std::endl;
+			std::shared_ptr<std::vector<unsigned char>>
 			data(new std::vector<unsigned char>);
-		    data->resize(response.audio_out().audio_data().length());
-		    memcpy(&((*data)[0]), response.audio_out().audio_data().c_str(), response.audio_out().audio_data().length());
-		    audio_output->Send(data);
+			data->resize(response.audio_out().audio_data().length());
+			memcpy(&((*data)[0]), response.audio_out().audio_data().c_str(), response.audio_out().audio_data().length());
+			audio_output->Send(data);
 		}
 
 		// CUSTOMIZE: render spoken request on screen
 		for (int i = 0; i < response.speech_results_size(); i++) {
-		    google::assistant::embedded::v1alpha2::SpeechRecognitionResult result =response.speech_results(i);
-		    if (verbose) {
-			std::clog << "<==AssistResponse.speech_results[]: \n"
-					  << result.transcript() << " ("
-					  << std::to_string(result.stability())
-					  << ")" << std::endl;
-		    }
+			google::assistant::embedded::v1alpha2::SpeechRecognitionResult result =response.speech_results(i);
+			if (verbose) {
+				std::clog << "<==AssistResponse.speech_results[]: \n"
+					<< result.transcript() << " ("
+					<< std::to_string(result.stability())
+					<< ")" << std::endl;
+			}
 		}
 		if (response.dialog_state_out().supplemental_display_text().size() > 0) {
-		    // CUSTOMIZE: render spoken response on screen
-		    std::clog << "<==AssistResponse.dialog_state_out.supplemental_display_text:" << std::endl;
-		    std::cout << response.dialog_state_out().supplemental_display_text()
-					<< std::endl;
+			// CUSTOMIZE: render spoken response on screen
+			std::clog << "<==AssistResponse.dialog_state_out.supplemental_display_text:" << std::endl;
+			std::cout << response.dialog_state_out().supplemental_display_text()
+				<< std::endl;
 		}
 	}
 
@@ -306,64 +300,63 @@ bool StartDialog(std::string locale,
 	// Destroy the stream
 	grpc::Status status = stream->Finish();
 	if (!status.ok()) {
-	    // Report the RPC failure.
-	    std::cerr << "assistant_sdk failed, error: " << status.error_message() << std::endl;
+		// Report the RPC failure.
+		std::cerr << "assistant_sdk failed, error: " << status.error_message() << std::endl;
 	}
 	// Stop the Audio Output Thread
 	audio_output->Stop();
-        return b_cont;
+	return b_cont;
 }
 
 int main(int argc, char** argv) {
-  std::string audio_input_source, text_input_source, credentials_file_path, credentials_type,
-              api_endpoint, locale;
-  bool b_cont = true;
-  // Initialize gRPC and DNS resolvers
-  // https://github.com/grpc/grpc/issues/11366#issuecomment-328595941
-  grpc_init();
-  if (!GetCommandLineFlags(argc, argv, &audio_input_source, &text_input_source,
-                           &credentials_file_path, &credentials_type,
-                           &api_endpoint, &locale)) {
-    return -1;
-  }
+	std::string audio_input_source, text_input_source, credentials_file_path, credentials_type, api_endpoint, locale;
+	bool b_cont = true;
+	// Initialize gRPC and DNS resolvers
+	// https://github.com/grpc/grpc/issues/11366#issuecomment-328595941
+	grpc_init();
+	if (!GetCommandLineFlags(argc, argv, &audio_input_source, &text_input_source,
+		&credentials_file_path, &credentials_type,
+		&api_endpoint, &locale)) {
+		return -1;
+	}
 
-  // Read credentials file.
-  std::ifstream credentials_file(credentials_file_path);
-  if (!credentials_file) {
-    std::cerr << "Credentials file \"" << credentials_file_path
-              << "\" does not exist." << std::endl;
-    return -1;
-  }
-  std::stringstream credentials_buffer;
-  credentials_buffer << credentials_file.rdbuf();
-  std::string credentials = credentials_buffer.str();
-  std::shared_ptr<CallCredentials> call_credentials;
-  call_credentials = grpc::GoogleRefreshTokenCredentials(credentials);
-  if (call_credentials.get() == nullptr) {
-    std::cerr << "Credentials file \"" << credentials_file_path
-              << "\" is invalid. Check step 5 in README for how to get valid "
-              << "credentials." << std::endl;
-    return -1;
-  }
+	// Read credentials file.
+	std::ifstream credentials_file(credentials_file_path);
+	if (!credentials_file) {
+		std::cerr << "Credentials file \"" << credentials_file_path
+			<< "\" does not exist." << std::endl;
+		return -1;
+	}
+	std::stringstream credentials_buffer;
+	credentials_buffer << credentials_file.rdbuf();
+	std::string credentials = credentials_buffer.str();
+	std::shared_ptr<CallCredentials> call_credentials;
+	call_credentials = grpc::GoogleRefreshTokenCredentials(credentials);
+	if (call_credentials.get() == nullptr) {
+		std::cerr << "Credentials file \"" << credentials_file_path
+			<< "\" is invalid. Check step 5 in README for how to get valid "
+			<< "credentials." << std::endl;
+		return -1;
+	}
 
-  // Begin a stream.
+	// Begin a stream.
 
-  auto channel = CreateChannel(api_endpoint);
-  std::shared_ptr<EmbeddedAssistant::Stub> assistant(
-      EmbeddedAssistant::NewStub(channel));
-  std::shared_ptr<AudioOutputALSA> audio_output(new AudioOutputALSA());
+	auto channel = CreateChannel(api_endpoint);
+	std::shared_ptr<EmbeddedAssistant::Stub> assistant(
+		EmbeddedAssistant::NewStub(channel));
+	std::shared_ptr<AudioOutputALSA> audio_output(new AudioOutputALSA());
 
-  while(1){
-      KeywordDetect detect;
-      detect.InitSNSR();
-      detect.Start();
-      detect.Loop();
-      detect.Stop();
-      b_cont = true;
+	while(1){
+		KeywordDetect detect;
+		detect.InitSNSR();
+		detect.Start();
+		detect.Loop();
+		detect.Stop();
+		b_cont = true;
 
-      while(b_cont) {
-          b_cont = StartDialog(locale, assistant, call_credentials, audio_output);
-      }
-  }
-  return 0;
+		while(b_cont) {
+			b_cont = StartDialog(locale, assistant, call_credentials, audio_output);
+		}
+	}
+	return 0;
 }
